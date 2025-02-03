2d83b1e2f062a54a2924847f9e3c647ee4739c75
ForhadRoben <i.rforhad@gmail.com>
2025-02-03 14:31:24 +0600

    project-config with mongoose

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import app from "./app";
+import config from "./app/config";
+// import dotenv from "dotenv";
+// dotenv.config();
+
+async function main() {
+  try {
+    await mongoose.connect(config.database_url as string);
+
+    app.listen(config.port, () => {
+      console.log(`server is listening at ${config.port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+main();
